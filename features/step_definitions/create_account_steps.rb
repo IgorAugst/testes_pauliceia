bd574bd3da20b7f47f39f6b9de5a7d709bb499b8
Kaynnan Felix <41689526+KaynnanFelix@users.noreply.github.com>
2023-12-09 23:37:27 -0300

    Update create_account_steps.rb

--- features/step_definitions/create_account_steps.rb
+++ features/step_definitions/create_account_steps.rb
@@ -13,6 +13,9 @@ And(/^I agree to the terms of service$/) do
   find(:xpath, '/html/body/div/section/div/div/section/div[1]/div[1]/form/div[3]/label[2]', visible: false).set(true)
 end
 
+And(/^I uncheck receive email notification$/) do
+  find(:xpath, '/html/body/div/section/div/div/section/div[1]/div[1]/form/div[3]/label[1]/span[1]', visible: false).set(false)
+end
 
 And(/^I click on the Cadastrar button$/) do
   find(:xpath, '/html/body/div[1]/section/div/div/section/div[1]/div[1]/form/button').click
@@ -21,4 +24,8 @@ end
 
 Then(/^I should receive a "([^"]*)" message$/) do |message|
   expect(page).to have_content(message)
+end
+
+Then(/^I shouldn't be able to click on the Cadastrar button$/) do
+  expect(page).to have_content('Cadastrar', disabled: true)
 end
