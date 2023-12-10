9e32eb7963fcd3a89b6679ce36f69f96b571c62b
Kaynnan Felix <41689526+KaynnanFelix@users.noreply.github.com>
2023-12-09 23:41:53 -0300

    Update change_language.rb

--- features/step_definitions/change_language.rb
+++ features/step_definitions/change_language.rb
@@ -1,7 +1,15 @@
+Given(/^I am on the home page$/) do
+  visit '/portal/home'
+end
+
 When(/^I click on the language button$/) do
   find(:xpath, '/html/body/div[1]/section/div/header/nav/div/div[2]/i[2]').click
 end
 
 Then(/^the language should change to english$/) do
   expect(page).to have_content('Look for an Address')
+end
+
+Then(/^the language should change to Portuguese$/) do
+  expect(page).to have_content('Quando ficará pronta?')
 end
