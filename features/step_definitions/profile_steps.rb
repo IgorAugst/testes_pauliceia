37e18d891a18b39e1fe96ba90b95e804925d718a
Kaynnan Felix <41689526+KaynnanFelix@users.noreply.github.com>
2023-12-10 11:13:30 -0300

    Update profile_steps.rb

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -10,6 +10,16 @@ When(/^I edit the user name for "([^"]*)"$/) do |username|
   find(:xpath, '/html/body/div/section/div/div/div[2]/div/div/div/form/div/div[2]/div[3]/input').set(username)
 end
 
+And(/^I press Profile$/) do
+  find(:xpath, '/html/body/div[1]/section/div/header/nav/div/div[1]/div/button/div').click
+  sleep(5)
+end
+
+And(/^I press My Profile$/) do
+  find(:xpath, '/html/body/div[2]/div/ul/li[1]/a/div').click
+  sleep(5)
+end
+
 And(/^I press Submit$/) do
   find(:xpath, '/html/body/div/section/div/div/div[2]/div/div/div/form/div/div[3]/div/a').click
   sleep(5)
